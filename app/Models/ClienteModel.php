2b18e1549f8f682d4e49f21c6daddad04707dc72
Damassa <felipelealdamasceno@gmail.com>
2025-11-10 13:03:46 -0300

    Criação do model de Cliente

---
+++ app/Models/ClienteModel.php
@@ -0,0 +1,53 @@
+<?php
+
+namespace App\Models;
+
+use CodeIgniter\Model;
+
+class ClienteModel extends Model
+{
+    protected $table            = 'clientes';
+    protected $primaryKey       = 'id';
+    protected $useAutoIncrement = true;
+    protected $returnType       = 'array';
+    protected $useSoftDeletes   = true;
+    protected $protectFields    = true;
+    protected $allowedFields    = [
+        'nome',
+        'sobrenome',
+        'imagem',
+    ];
+
+    protected bool $allowEmptyInserts = false;
+    protected bool $updateOnlyChanged = true;
+
+    protected array $casts = [];
+    protected array $castHandlers = [];
+
+    // Dates
+    protected $useTimestamps = true;
+    protected $dateFormat    = 'datetime';
+    protected $createdField  = 'created_at';
+    protected $updatedField  = 'updated_at';
+    protected $deletedField  = 'deleted_at';
+
+    // Validation
+    protected $validationRules      = [
+        'nome' => 'required|min_length[3]',
+        'sobrenome' => 'required|min_length[3]',
+    ];
+    protected $validationMessages   = [];
+    protected $skipValidation       = false;
+    protected $cleanValidationRules = true;
+
+    // Callbacks
+    protected $allowCallbacks = true;
+    protected $beforeInsert   = [];
+    protected $afterInsert    = [];
+    protected $beforeUpdate   = [];
+    protected $afterUpdate    = [];
+    protected $beforeFind     = [];
+    protected $afterFind      = [];
+    protected $beforeDelete   = [];
+    protected $afterDelete    = [];
+}
